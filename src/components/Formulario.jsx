089bd1512ef054c4010c906225025fcb41dd01cd
Facundo Saavedra <facusaavedrx@gmail.com>
2023-01-19 10:33:32 -0300

    Se creo state para gatos

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,6 +1,15 @@
+import { useState } from 'react'
+
 function Formulario () {
+  const [nombre, guardarNombre] = useState('')
+  const [cantidad, guardarCantidad] = useState(0)
+  const handleSubmit = e => {
+    e.preventDefault()
+  }
   return (
-    <form>
+    <form
+      onSubmit={handleSubmit}
+    >
       <h2>Agrega tus gastos aquí</h2>
       <div className='campo'>
         <label>Nombre gasto</label>
@@ -8,6 +17,8 @@ function Formulario () {
           type='text'
           className='u-full-width'
           placeholder='Ejemplo: Transporte'
+          value={nombre}
+          onChange={e => guardarNombre(e.target.value)}
         />
       </div>
       <div className='campo'>
@@ -16,6 +27,7 @@ function Formulario () {
           type='number'
           className='u-full-width'
           placeholder='Ejemplo: 300'
+          onChange={e => guardarCantidad(Number(e.target.value))}
         />
       </div>
       <input
